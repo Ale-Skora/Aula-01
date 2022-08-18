d1325ecc16b53a7a02d7032a73980eccc736b443
Ale_Skora <ale.skora1@gmail.com>
2022-08-18 18:51:15 -0300

    ..

---
+++ src/main/java/org/example/Aluno.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.time.LocalDate;
+
+public class Aluno {
+
+    private String nome;
+    private String matricula;
+    private LocalDate dataNascimento;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
+
---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        Aluno aluno = new Aluno ();
+        System.out.println(LocalDate.now());
+        System.out.println(LocalDateTime.now());
+        System.out.println(LocalDateTime.now());
+
+
+
+
+
+    }
+}
